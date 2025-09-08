d00ae4624f6a489d99d6abb9bb883045d8444c72
Rahul Verma <110929992+rahulverma012@users.noreply.github.com>
2025-09-09 01:49:15 +0530

    Update run.sh

--- 02-Analysis/run.sh
+++ 02-Analysis/run.sh
@@ -9,10 +9,10 @@
 makeSound()  { for i in $(seq 1 5); do echo -en "\a"; sleep 0.25; done ; }
 checkToken() { tokenStatus=$(alien-token-info 2>&1 | tail -1)  # Capture the output of alien-token-info
   if [[ "$tokenStatus" == "File >>><<< not found" ]];then
-    echo "$(date) ERROR :: Token Not Found, Create the token" ; makeSound
+    echo "$(date) ❌ ERROR :: Token Not Found, Create the token" ; makeSound
     exit 1  #stop execution if token not found
   elif [[ "$tokenStatus" == *"alien-token-info: command not found" ]]; then
-    echo "ERROR :: alien-token-info command not found"
+    echo "❌ ERROR :: alien-token-info command not found"
     echo "do    ==> alienv load  O2Physics/latest"
     echo "or do ==> alienv enter O2Physics/latest"
     echo "or do ==> alienv load  O2Physics/latest ninja/latest"
@@ -20,7 +20,7 @@ checkToken() { tokenStatus=$(alien-token-info 2>&1 | tail -1)  # Capture the out
     makeSound
     exit 1  #stop execution if environment is not loaded
   else
-    echo "Token Found, Processing further ..."
+    echo "✅ Token Found, Processing further ..."
   fi
 }
 buildO2Task(){
@@ -29,7 +29,7 @@ buildO2Task(){
   cd "$ALIBUILD_WORK_DIR"/BUILD/O2Physics-latest/O2Physics/
   time ninja install &> output-01.log;
   if [[ $(tail -1 output-01.log) == *Up-to-date* ]] ; then
-     echo "BUILD SUCESSFULL"
+     echo "✅ BUILD SUCESSFULL. Processing further ..."
   else
      if tail -1 output-01.log | grep -q "ninja: command not found"; then
      echo "ninja: command not found"
@@ -38,7 +38,7 @@ buildO2Task(){
      makeSound
      exit 1
      else
-     echo "BUILD FAILED" ; code output-01.log ; makeSound
+     echo "❌ BUILD FAILED" ; code output-01.log ; makeSound
      exit 1 #stop execution if build failed
      fi
   fi
