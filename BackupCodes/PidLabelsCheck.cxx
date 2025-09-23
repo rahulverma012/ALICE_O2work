e005dda8e9f73eb27ff5e8db46a8d6f1024289d1
Rahul Verma <110929992+rahulverma012@users.noreply.github.com>
2025-09-24 03:55:53 +0530

    Create PidLabelsCheck.cxx

---
+++ BackupCodes/PidLabelsCheck.cxx
@@ -0,0 +1,22 @@
+    if( kK0Short    != 310  ) {LOG(fatal)<<" PDG code is not correct :: kK0Short != 310 :: kK0Short = "<<kK0Short;}
+    if( kLambda0    != 3122 ) {LOG(fatal)<<" PDG code is not correct :: kLambda0 != 3122 :: kLambda0 = "<<kLambda0;}
+    if( kLambda0Bar !=-3122 ) {LOG(fatal)<<" PDG code is not correct :: kLambda0Bar != -3122 :: kLambda0Bar = "<<kLambda0Bar;} 
+    if( kGamma      != 22   ) {LOG(fatal)<<" PDG code is not correct :: kGamma != 22 :: kGamma = "<<kGamma;}
+
+    if( kPiPlus     != 211  ) {LOG(fatal)<<" PDG code is not correct :: kPiPlus != 211 :: kPiPlus = "<<kPiPlus;}
+    if( kPiMinus    != -211 ) {LOG(fatal)<<" PDG code is not correct :: kPiMinus != -211 :: kPiMinus = "<<kPiMinus;}
+    if( kKPlus      !=  321 ) {LOG(fatal)<<" PDG code is not correct :: kKPlus != 321 :: kKPlus = "<<kKPlus;}
+    if( kKMinus     != -321 ) {LOG(fatal)<<" PDG code is not correct :: kKMinus != -321 :: kKMinus = "<<kKMinus;}
+    if( kProton     != 2212 ) {LOG(fatal)<<" PDG code is not correct :: kProton != 2212 :: kProton = "<<kProton;}
+    if( kProtonBar  != -2212) {LOG(fatal)<<" PDG code is not correct :: kProtonBar != -2212 :: kProtonBar = "<<kProtonBar;}
+    if( kPositron   != -11  ) {LOG(fatal)<<" PDG code is not correct :: kPositron != -11 :: kPositron = "<<kPositron;}    
+    if( kElectron   !=  11  ) {LOG(fatal)<<" PDG code is not correct :: kElectron != 11 :: kElectron = "<<kElectron;}
+    if( kMuonMinus  !=  13  ) {LOG(fatal)<<" PDG code is not correct :: kMuonMinus != 13 :: kMuonMinus = "<<kMuonMinus;}
+    if( kMuonPlus   != -13  ) {LOG(fatal)<<" PDG code is not correct :: kMuonPlus != -13 :: kMuonPlus = "<<kMuonPlus;}
+    if( kDeuteron   != 1000010020 ) {LOG(fatal)<<" PDG code is not correct :: kDeuteron != 1000010020 :: kDeuteron = "<<kDeuteron;}
+
+    if( kPhi        != 333  ) {LOG(fatal)<<" PDG code is not correct ::  kPhi != 333 :: kPhi = "<<kPhi;}
+    if( kJPsi       != 443  ) {LOG(fatal)<<" PDG code is not correct ::  kJPsi != 443 :: kJPsi = "<<kJPsi;}
+    if( kK0Star892  != 313  ) {LOG(fatal)<<" PDG code is not correct ::  kK0Star892 != 313 :: kK0Star892 = "<<kK0Star892;}
+    if( kLambda1520 != 3124 ) {LOG(fatal)<<" PDG code is not correct ::  kLambda1520 != 3124 :: kLambda1520 = "<<kLambda1520;}
+    if( kRho770_0   != 113  ) {LOG(fatal)<<" PDG code is not correct ::  kRho770_0 != 113 :: kRho770_0 = "<<kRho770_0;}
