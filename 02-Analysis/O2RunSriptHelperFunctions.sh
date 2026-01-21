f926e2685aeee170757b6c2ecf20560a462d0d12
Rahul Verma <110929992+rahulverma012@users.noreply.github.com>
2026-01-22 04:58:53 +0530

    Implement helper functions for O2RunScript
    Add basic helper functions for token checks, internet connectivity, and file downloads.

--- 02-Analysis/O2RunSriptHelperFunctions.sh
+++ 02-Analysis/O2RunSriptHelperFunctions.sh
@@ -1,1 +1,141 @@
+#!/bin/bash
+# Author: [Rahul Verma]
+# basic functions for checking token, checking internet connection, building the packages, and making sound
 
+unsetHTTPproxy() {
+  echo "[unsetHTTPproxy]: HTTP and HTTPS proxies before unset"
+  env | grep prox
+  unset http_proxy https_proxy
+  echo "[unsetHTTPproxy]: HTTP and HTTPS proxies After unset"
+  echo "[unsetHTTPproxy]: HTTP and HTTPS proxies have been unset."
+}
+
+makeSound()  { for i in $(seq 1 5); do echo -en "\a"; sleep 0.25; done ; }
+
+getTime() {
+  if [ $# -eq 1 ]; then
+    # One argument: compute time elapsed since given start time
+    local start="$1"
+    local now=$(date +%s%6N)
+    local elapsed=$((now - start))
+    echo "$elapsed"
+  elif [ $# -eq 2 ]; then
+    # Two arguments: return time difference in microseconds
+    local start="$1"
+    local end="$2"
+    local elapsed=$((end - start))
+    echo "$elapsed"
+  else
+    echo "Usage:"
+    echo "  getTime <start>              # Shows elapsed time in microseconds"
+    echo "  getTime <start> <end>        # Shows difference in microseconds"
+    return 1
+  fi
+}
+
+microToSeconds() {
+  local microseconds="$1"
+  awk "BEGIN {printf \"%.4f\", $microseconds / 1000000}"
+}
+
+checkToken() { tokenStatus=$(alien-token-info 2>&1 | tail -1)  # Capture the output of alien-token-info
+  if [[ "$tokenStatus" == "File >>><<< not found" ]];then
+    echo "$(date) ❌ ERROR :: Token Not Found, Create the token" ; makeSound
+    exit 1  #stop execution if token not found
+  elif [[ "$tokenStatus" == *"alien-token-info: command not found" ]]; then
+    echo "❌ ERROR :: alien-token-info command not found"
+    echo "do    ==> alienv load  O2Physics/latest"
+    echo "or do ==> alienv enter O2Physics/latest"
+    echo "or do ==> alienv load  O2Physics/latest ninja/latest"
+    echo "or do ==> alienv enter O2Physics/latest ninja/latest"
+    makeSound
+    exit 1  #stop execution if environment is not loaded
+  else
+    echo "✅ Token Found, Processing further ..."
+  fi
+}
+buildO2Task(){
+  echo "BUILDING EXECUTABLE"
+  currentDir=$(pwd)
+  cd "$ALIBUILD_WORK_DIR"/BUILD/O2Physics-latest/O2Physics/
+  time ninja install &> output-01.log;
+  if [[ $(tail -1 output-01.log) == *Up-to-date* ]] ; then
+     echo "✅ BUILD SUCESSFULL. Processing further ..."
+  else
+     if tail -1 output-01.log | grep -q "ninja: command not found"; then
+     echo "ninja: command not found"
+     echo "do    ==> alienv enter O2Physics/latest ninja/latest"
+     echo "or do ==> alienv load  O2Physics/latest ninja/latest"
+     makeSound
+     exit 1
+     else
+     echo "❌ BUILD FAILED" ; code output-01.log ; makeSound
+     exit 1 #stop execution if build failed
+     fi
+  fi
+  cd $currentDir
+}
+checkInternet(){ STATUS=$(curl -s --max-time 3 -o /dev/null -w "%{http_code}" http://captive.apple.com)
+  if [ "$STATUS" -eq "200" ]; then
+    echo "✅ Internet access available. Processing further ..."
+  else
+    echo "❌ Internet blocked or login required. HTTP Status: $STATUS"
+    makeSound
+    exit 1
+  fi
+}
+
+downloadAliceFiles(){
+  local alicePathList="input_ALICEPathList.txt"
+  local localPathList="input_ALICEPathToLocalFile.txt"
+  local alienStorage="@ALICE::CERN::EOS"
+  local fileList="input_data.txt"
+  local fileStat="input_data.stat"
+
+  # Store inputList in an array
+  local FileArray=()
+  while IFS= read -r line || [[ -n $line ]]; do     #[[ -n $line ]]  will capture the last line if the file ends without new line  
+      FileArray+=("$line")                       #Read from the file list and append the FileList
+  done < "$alicePathList"
+
+  local localPathArray=()
+  while IFS= read -r line || [[ -n $line ]]; do     #[[ -n $line ]]  will capture the last line if the file ends without new line  
+      localPathArray+=("$line")                       #Read from the file list and append the FileList
+  done < "$localPathList"
+
+  if [ ${#FileArray[@]} != ${#localPathArray[@]} ]; then
+    echo "[downloadAliceFiles]: ❌ ERROR :: #FileArray[@] != #localPathArray[@] i.e. ${#FileArray[@]} != ${#localPathArray[@]}"
+    exit
+  fi
+
+  echo 
+  echo "Files to be downloaded :: nFiles = ${#FileArray[@]}"
+  # Print all lines
+  echo 
+  local TotalTimeStart=$(date +%s%6N)
+  local whereis_TotalTime=0
+  local cp_TotalTime=0
+  echo "FILE DOWNLOAD START ..."
+  for (( i=0; i< ${#FileArray[@]}; i++ )) ; do
+    echo "[downloadAliceFiles]: $((i+1))/${#FileArray[@]} :: ${FileArray[i]}  ==> ${localPathArray[i]}"
+    echo "[downloadAliceFiles]: $((i+1))/${#FileArray[@]} :: Downlaoding..."
+    filePath=${FileArray[$i]}
+    localPath=${localPathArray[$i]}
+
+    whereis_TimeStart=$(date +%s%6N)
+    alien.py whereis $filePath >> $fileStat
+    ((whereis_TotalTime+= $(getTime "$whereis_TimeStart")))
+    local cp_TimeStart=$(date +%s%6N)
+    if alien.py cp "$filePath""$alienStorage" "file::$localPath" ; then
+      echo "File Download Succesfull"
+      echo $localPath >> $fileList
+    else
+      echo "File Downlaod Fail"
+    fi
+    ((cp_TotalTime+= $(getTime "$cp_TimeStart")))
+  done
+  
+  echo "FILE DOWNLOAD OVER :: alien.py cp Time = $(microToSeconds $cp_TotalTime ) seconds"
+  echo "FILE DOWNLOAD OVER :: whereIs     Time = $(microToSeconds $whereis_TotalTime ) seconds"
+  echo "FILE DOWNLOAD OVER :: total       Time = $(microToSeconds "$(getTime "$TotalTimeStart")") seconds"
+}
